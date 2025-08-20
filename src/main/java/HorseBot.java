ff1f5d584368424cbef88b07a7434b6fcbf632f8
tam308 <joshuatamkayui@gmail.com>
2025-08-20 21:01:53 +0800

    Add functionality to echo user commands and exit on bye

--- src/main/java/HorseBot.java
+++ src/main/java/HorseBot.java
@@ -1,20 +1,43 @@
+import java.util.Scanner;
 public class HorseBot {
-    public static void main(String[] args) {
-        String logo = ".--------------------------------------------------------------------.\n" +
-                "| __  __                                     ____            __      |\n" +
-                "|/\\ \\/\\ \\                                   /\\  _`\\         /\\ \\__   |\n" +
-                "|\\ \\ \\_\\ \\    ___   _ __   ____     __      \\ \\ \\L\\ \\    ___\\ \\ ,_\\  |\n" +
-                "| \\ \\  _  \\  / __`\\/\\`'__\\/',__\\  /'__`\\     \\ \\  _ <'  / __`\\ \\ \\/  |\n" +
-                "|  \\ \\ \\ \\ \\/\\ \\L\\ \\ \\ \\//\\__, `\\/\\  __/      \\ \\ \\L\\ \\/\\ \\L\\ \\ \\ \\_ |\n" +
-                "|   \\ \\_\\ \\_\\ \\____/\\ \\_\\\\/\\____/\\ \\____\\      \\ \\____/\\ \\____/\\ \\__\\|\n" +
-                "|    \\/_/\\/_/\\/___/  \\/_/ \\/___/  \\/____/       \\/___/  \\/___/  \\/__/|\n" +
-                "'--------------------------------------------------------------------'";
-        String line = "______________________________________________________________________";
+    static String line = "______________________________________________________________________";
+    //print the chatbot's introductory message
+    public static void intro(){
+        String logo = """
+                .--------------------------------------------------------------------.
+                | __  __                                     ____            __      |
+                |/\\ \\/\\ \\                                   /\\  _`\\         /\\ \\__   |
+                |\\ \\ \\_\\ \\    ___   _ __   ____     __      \\ \\ \\L\\ \\    ___\\ \\ ,_\\  |
+                | \\ \\  _  \\  / __`\\/\\`'__\\/',__\\  /'__`\\     \\ \\  _ <'  / __`\\ \\ \\/  |
+                |  \\ \\ \\ \\ \\/\\ \\L\\ \\ \\ \\//\\__, `\\/\\  __/      \\ \\ \\L\\ \\/\\ \\L\\ \\ \\ \\_ |
+                |   \\ \\_\\ \\_\\ \\____/\\ \\_\\\\/\\____/\\ \\____\\      \\ \\____/\\ \\____/\\ \\__\\|
+                |    \\/_/\\/_/\\/___/  \\/_/ \\/___/  \\/____/       \\/___/  \\/___/  \\/__/|
+                '--------------------------------------------------------------------'""";
         //ascii art generated with https://www.asciiart.eu/text-to-ascii-art
         System.out.println(line);
         System.out.println(logo + "\nNeigh! I'm a Horse! How can i assist you?");
         System.out.println(line);
-        System.out.println("Bye bye! Neigh-ce to meet you!");
+}
+
+    //echo user input with indentation
+    public static void echo(){
+        String userInput = "";
+        while (!userInput.equals("bye")) {
+            Scanner in = new Scanner(System.in);
+            userInput = in.nextLine();
+            if (!userInput.equals("bye")) {
+                System.out.println(line);
+                System.out.println("    " + userInput);
+                System.out.println(line);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        intro();
+        echo();
+        System.out.println(line);
+        System.out.println("Bye bye! It was Neigh-ce to meet you!");
         System.out.println(line);
     }
 }
